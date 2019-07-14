50b050429a67e107fea05112ae3632342de335f4
Mendel Stillerman <menstiler@gmail.com>
2019-07-14 19:42:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployees = function(array) {
+    return array.map(function(el){
+        return createEmployeeRecord(el)
+    })
+}
+
+let createTimeInEvent = function(datestamp){
+  let [date, hour] = datestamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let createTimeOutEvent = function(datestamp){
+    let [date, hour] = datestamp.split(' ')
+    // let hour = date.split(' ')[1]
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(date){
+    let timeIn = this.timeInEvents.find(function(e){
+        return e.date === date
+    })
+
+    let timeOut = this.timeOutEvents.find(function(e){
+        return e.date === date
+    })
+
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(date){
+    let dates = hoursWorkedOnDate.call(this, date)
+    let wages = dates * this.payPerHour
+    return parseFloat(wages.toString())
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -14,9 +73,27 @@ let allWagesFor = function () {
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
+    let payment = eligibleDates.reduce(function (total, date) {
+        return total + wagesEarnedOnDate.call(this, date)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+    return payment
+}
+
+let createEmployeeRecords = function(array) {
+  return array.map(function(array){
+    return createEmployeeRecord(array)
+  })
+}
+
+let findEmployeebyFirstName = function(array, firstName) {
+  return array.find(function(el){
+    return el.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(total, el){
+        return total + allWagesFor.call(el)
+    }, 0)
 }
